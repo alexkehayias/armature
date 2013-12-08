167921c4fbb1454a5c248b5b2ea1c557b045a277
Alex Kehayias <alex.kehayias@gmail.com>
2013-12-08 17:38:34 -0500

    Use mk-chan when creating any channels so subscribers can be added/removed on the fly

--- src/cljs/armature/channels.cljs
+++ src/cljs/armature/channels.cljs
@@ -6,6 +6,8 @@
 ;; TODO a fan out channel that takes a mutable list of channels to
 ;; send messages to before removing the messsage
 
+
+
 (defn mk-loop
   "Create a loop over a channel calling fn on every message"
   [ch f]
@@ -15,10 +17,33 @@
   ch)
 
 (defn fan-out-channel
-  "Returns a channel where messages are also enqueued to all channels
-   in the subscribers atom"
+  "Returns a hashmap channel and subscribers where messages are also enqueued 
+   to all channels in the subscribers atom"
   [subscribers]
-  (mk-loop (chan) #(doseq [sub @subscribers] (go (>! sub %)))))
+  (mk-loop (chan) #(doseq [sub @subscribers] (go (>! (:channel sub) %)))))
+
+(defn mk-chan []
+  (let [subscribers (atom [])
+        ch (fan-out-channel subscribers)]
+    {:channel ch :subscribers subscribers :id (gensym "chan__")}))
+
+(defn subscribe
+  "Subscribe ch1 to ch2 such that each message that goes to ch1 goes to ch2
+   Optionally pass in a function that takes one arg used to filter messages."
+  [pub-ch sub-ch & {:keys [f] :or {f nil}}]
+  ;; TODO allow a filter function to control what goes in to the chan
+  (swap! (:subscribers pub-ch) conj sub-ch))
+
+(defn unsubscribe
+  [pub-chan sub-chan]
+  ;; TODO filter for the id of the chan and remove it
+  ;;(swap! (:subscribers chan) sub-chan)
+  )
+
+(defn publish
+  "Asnychronously publish a message, msg, to channel ch"
+  [ch msg]
+  (go (>! (:channel ch) msg)))
 
 (defn consume-every 
   "Consume every message from channel ch that matches event-id selector
@@ -32,11 +57,14 @@
                             "on" ch-name)
                      (f %)))))
 
-;; Test for fan out
-;; (def ch1 (chan))
-;; (def ch2 (chan))
-;; (def test-fan-out
-;;   (fan-out-channel (atom [ch1 ch2])))
-;; (mk-loop ch1 #(debug "ch1 recieved msg" %))
-;; (mk-loop ch2 #(debug "ch2 recieved msg" %))
-;; (go (>! test-fan-out "test"))
+
+
+;;Test for fan out
+(def pub1 (mk-chan))
+(def ch1 (mk-chan))
+(def ch2 (mk-chan))
+(subscribe pub1 ch1)
+(subscribe pub1 ch2)
+(mk-loop (:channel ch1) #(debug "ch1 recieved msg" %))
+(mk-loop (:channel ch2) #(debug "ch2 recieved msg" %))
+(publish pub1 "test")
