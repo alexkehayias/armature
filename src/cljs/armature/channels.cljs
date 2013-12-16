d6ca8732574ec72f9510c7900d0ca4501cef1f76
Alex Kehayias <alex.kehayias@gmail.com>
2013-12-15 23:38:31 -0500

    Don't make publish function perform two duties.

--- src/cljs/armature/channels.cljs
+++ src/cljs/armature/channels.cljs
@@ -13,23 +13,27 @@
   ch)
 
 (defn fan-out-channel
-  "Returns a channel where messages are also enqueued 
-   to all channels in the subscribers atom"
+  "Returns a pair of channels, in and out, where messages are also enqueued 
+   to all channels in the subscribers atom.
+
+   Subscribers can be swap!ed to add new subscribers at any time"
   [subscribers]
-  (let [in-ch (chan)
-        out-ch (chan)]
-    (mk-loop in-ch #(do (when-not (empty? @subscribers)
+  (let [in (chan)
+        out (chan)]
+    (mk-loop in #(do (when-not (empty? @subscribers)
                           (doseq [sub @subscribers]
                             (debug "enqueuing" % "to" sub)
                             (publish sub %)))
-                        ;; We always publish to the out channel
-                        (publish out-ch %)))
-    [in-ch out-ch]))
+                     ;; We always publish to the out channel so that
+                     ;; all messages don't get consumed by the
+                     ;; subscribers only
+                     (go (>! out %))))
+    [in out]))
 
 (defn mk-chan [& {:keys [subscribers] :or {subscribers nil}}]
   (let [subs (atom subscribers)
-        [in-ch out-ch] (fan-out-channel subs)]
-    {:in in-ch :out out-ch :subscribers subs :id (gensym "chan__")}))
+        [in out] (fan-out-channel subs)]
+    {:in in :out out :subscribers subs :id (gensym "chan__")}))
 
 (defn subscribe
   "Subscribe ch1 to ch2 such that each message that goes to ch1 goes to ch2
@@ -46,11 +50,10 @@
   sub-ch)
 
 (defn publish
-  "Asnychronously publish a message, msg, to channel ch"
+  "Asnychronously publish a message, msg, to channel ch.
+   Checks if there is an :in for the channel otherwise enqueues to the channel"
   [ch msg]
-  (if (:in ch)
-    (go (>! (:in ch) msg))
-    (go (>! ch msg))))
+  (go (>! (:in ch) msg)))
 
 (defn consume-every 
   "Consume every message from channel ch that matches event-id selector
@@ -71,9 +74,9 @@
 (def ch1 (mk-chan))
 (def ch2 (mk-chan))
 (def ch3 (mk-chan))
-
 (def pub1 (mk-chan :subscribers [ch1]))
-(publish pub1 "test2")
+(subscribe ch1 ch2)
+(publish pub1 "HELLO WORLD")
 ;; This channel already has a function called in a loop on it's input,
 ;; we need to embed our function to the subscribe
 ;; (consume-every "testing"
@@ -83,7 +86,7 @@
 
 
 (mk-loop (:out ch1) #(debug "ch1 recieved msg" %))
-;;(mk-loop (:channel ch2) #(debug "ch2 recieved msg" %))
+(mk-loop (:out ch2) #(debug "ch2 recieved msg" %))
 ;;(mk-loop (:channel ch3) #(debug "ch3 recieved msg" %))
 
 ;;(unsubscribe pub1 ch1)
